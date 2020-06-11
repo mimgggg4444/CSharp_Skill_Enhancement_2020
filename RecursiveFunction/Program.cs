30328b619ba12ffdc20b7f5ff5ab2032b5c37dfb
a <a@DESKTOP-231MPSD>
2020-06-12 08:11:53 +0900

    sumRecursive

---
+++ RecursiveFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RecursiveFunction
+{
+    class Program
+    {
+        static uint SumRecursive(uint num)
+        {
+            if(num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return num + SumRecursive(num - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumRecursive(123151));
+
+        }
+    }
+}
