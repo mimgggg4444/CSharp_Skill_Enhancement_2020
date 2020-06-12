42b9400e3b6228ce4b4cf9a647ba14ad7dad3dbb
a <a@DESKTOP-231MPSD>
2020-06-13 01:01:10 +0900

    UP!

--- listOfStudentsUsing2d/Program.cs
+++ listOfStudentsUsing2d/Program.cs
@@ -7,26 +7,43 @@ namespace listOfStudentsUsing2d
     {
         static void Main(string[] args)
         {
+            //const int CLASS_COUNT = 3;
+            //const int STUDENT_COUNT = 5;
+
+            //string[,] classrooms = new string[CLASS_COUNT, STUDENT_COUNT]
+            //{
+            //    {"sev","draco","bellatrix","","" },
+            //    {"cedric","hannah","","","" },
+            //    {"hermione","harry","leville","parvati","ron" }
+            //};
+
+            //for(int i = 0; i < CLASS_COUNT; i++)
+            //{
+            //    Console.WriteLine($"class{i + 1}Info***********************");
+
+            //    for (int j = 0; j < STUDENT_COUNT; ++j)
+            //    {
+            //        Console.WriteLine($"Student{j + 1} name: {classrooms[i, j]}");
+            //    }
+            //    Console.WriteLine("********************************");
+            //}
+
             const int CLASS_COUNT = 3;
-            const int STUDENT_COUNT = 5;
+            int[] STUDENT_COUNT_PER_CLASS = { 3, 2, 5 };
+            string[][] classrooms = new string[CLASS_COUNT][];
 
-            string[,] classrooms = new string[CLASS_COUNT, STUDENT_COUNT]
+            for (int i = 0; i < CLASS_COUNT; ++i)
             {
-                {"sev","draco","bellatrix","","" },
-                {"cedric","hannah","","","" },
-                {"hermione","harry","leville","parvati","ron" }
-            };
-
-            for(int i = 0; i < CLASS_COUNT; i++)
-            {
-                Console.WriteLine($"class{i + 1}Info***********************");
-
-                for (int j = 0; j < STUDENT_COUNT; ++j)
-                {
-                    Console.WriteLine($"Student{j + 1} name: {classrooms[i, j]}");
-                }
-                Console.WriteLine("********************************");
+                classrooms[i] = new string[STUDENT_COUNT_PER_CLASS[i]];
             }
+
+            int classIndex = 0;
+            int studentIndex = 0;
+
+            string[] studentNames = classrooms[classIndex];
+            studentNames[studentIndex] = "Kimkimkim";
+
+            Console.WriteLine($"class 1 - student 1: {classrooms[classIndex][studentIndex]}");
         }
     }
 }
