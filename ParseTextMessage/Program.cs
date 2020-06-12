d5d14a352d3b94ef27ed286c0aa7e0ef4e804f3b
a <a@DESKTOP-231MPSD>
2020-06-13 00:56:08 +0900

    UPUPUP!

---
+++ ParseTextMessage/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace ParseTextMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textMessage = File.ReadAllText(@"TextMessage.txt");
+
+            string[] lines = textMessage.Split('\n');
+
+            string[] dateTimeString = lines[1].Split(' ');
+            string nameOfDay = dateTimeString[0];
+
+            string[] date = dateTimeString[1].Split('-');
+
+            int year = int.Parse(date[0]);
+            int month = int.Parse(date[1]);
+            int day = int.Parse(date[2]);
+
+            string[] time = dateTimeString[2].Split(':');
+
+            int hours = int.Parse(time[0]);
+            int mins = int.Parse(time[1]);
+            float seconds = float.Parse(time[2]);
+
+            string email = lines[2].Trim();
+
+            string courseCode = lines[3].Replace("Course", "").Trim();
+            string term = lines[4].Replace("Term", "").Trim();
+
+            Console.WriteLine($"{nameOfDay}");
+            Console.WriteLine($"{year}");
+            Console.WriteLine($"{month}");
+            Console.WriteLine($"{hours}");
+            Console.WriteLine($"{mins}");
+            Console.WriteLine($"{seconds}");
+            Console.WriteLine($"{email}");
+            Console.WriteLine($"{courseCode}");
+            Console.WriteLine($"{term}");
+        }
+    }
+}
---
+++ listOfStudentsUsing2d/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace listOfStudentsUsing2d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CLASS_COUNT = 3;
+            const int STUDENT_COUNT = 5;
+
+            string[,] classrooms = new string[CLASS_COUNT, STUDENT_COUNT]
+            {
+                {"sev","draco","bellatrix","","" },
+                {"cedric","hannah","","","" },
+                {"hermione","harry","leville","parvati","ron" }
+            };
+
+            for(int i = 0; i < CLASS_COUNT; i++)
+            {
+                Console.WriteLine($"class{i + 1}Info***********************");
+
+                for (int j = 0; j < STUDENT_COUNT; ++j)
+                {
+                    Console.WriteLine($"Student{j + 1} name: {classrooms[i, j]}");
+                }
+                Console.WriteLine("********************************");
+            }
+        }
+    }
+}
