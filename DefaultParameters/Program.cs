dc7c0ee1ef3645d0b9c372fc592a8552a2f575bd
a <a@DESKTOP-231MPSD>
2020-06-13 00:11:10 +0900

    UP!!

---
+++ DefaultParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefaultParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bar();
+            Bar();
+            Bar();
+
+        }
+    
+        static void Bar(string s, string s2 = "", string s3 = "")
+        {
+            Console.WriteLine($"{s}, {s2}, {s3}");
+        }
+    }
+}
