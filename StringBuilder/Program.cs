b124855139addccc8ee62584c57ff4bd7a60d6ef
a <a@DESKTOP-231MPSD>
2020-06-13 21:58:16 +0900

    UP!

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder builder = new StringBuilder(4096);
+            builder.Append("Hello kim!");
+            builder.AppendLine(" What");
+            builder.Append("GOOD!");
+
+            string greetings = builder.ToString();
+            Console.WriteLine(greetings);
+        }
+    }
+}
