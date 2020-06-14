ceb03bfa3dee209a1862d9bb5532e0d00a681f84
a <a@DESKTOP-231MPSD>
2020-06-15 07:17:28 +0900

    UP!

---
+++ class/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class1
+{
+    public class Car
+    {
+        public int Price;
+        public float Gas;
+        public string Owner;
+
+        public int GetPrice()
+        {
+            return Price;
+        }
+        public string GetOwner()
+        {
+            return Owner;
+        }
+        public void SetOwner(string owner)
+        {
+            Owner = owner;
+        }
+        public float GetGas()
+        {
+            return Gas;
+        }
+        public void FillUp(float gas)
+        {
+            Gas += gas;
+        }
+        public void Move()
+        {
+            Gas -= 0.5f;
+        }
+        public void Honk()
+        {
+            Console.WriteLine("Hock~ Hock~");
+        }
+    }
+
+}
---
+++ class/Program.cs
@@ -0,0 +1,18 @@
+﻿using class1;
+using System;
+using System.Xml.Serialization;
+
+namespace classPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            car.Owner = "Kims";
+            Console.WriteLine(car);
+
+        }
+    }
+}
