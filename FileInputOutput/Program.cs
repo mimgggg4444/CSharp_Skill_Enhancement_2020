ce1bedebdbe638f6962371cc738eb83b429a3a94
a <a@DESKTOP-231MPSD>
2020-06-17 08:17:01 +0900

    아푸아푸!

---
+++ FileInputOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileInputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes1 = new byte[3] { 12, 65, 3 };
+            File.WriteAllBytes(@"C:\C#\start3",bytes1);
+
+            byte[] bytes2 = File.ReadAllBytes(@"");
+            for(int i = 0; i < bytes2.Length; ++i)
+            {
+                Console.WriteLine(bytes2[i]);
+            }
+
+
+        }
+    }
+}
---
+++ LINQ/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    class OrderItem
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public uint Quantity { get; set; }
+    }
+}
