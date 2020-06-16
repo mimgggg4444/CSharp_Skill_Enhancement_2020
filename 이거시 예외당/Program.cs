aae23103f6060d8372f3fab15d53f837c001b724
a <a@DESKTOP-231MPSD>
2020-06-17 08:28:23 +0900

    up

---
+++ 이거시 예외당/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace 이거시_예외당
+{
+    class Program
+    {
+        static void PrintLines(string path)
+        {
+            if(!File.Exists(path))
+            {
+                return;
+            }
+
+            string[] lines;
+            try
+            {
+                lines = File.ReadAllLines(path);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.Error.WriteLine($"file not found: {e}");
+                return;
+            }
+
+            foreach(var line in lines)
+            {
+                
+            }
+        }
+
+        static void Main(string[]args)
+        {
+            string path = @"C:\start";
+            PrintLines(path);
+
+        }
+            
+
+
+    }
+}
