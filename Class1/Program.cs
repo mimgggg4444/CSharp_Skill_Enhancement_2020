9a2b74ebb724014922fab9927755f4710fc0369c
a <a@DESKTOP-231MPSD>
2020-06-19 17:53:10 +0900

    up

---
+++ Class1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior warrior = new Warrior("minki");
+
+            warrior.Introduce();
+            warrior.GetStatus();
+
+            warrior.SwordStrike();
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
+            warrior.GetStatus();
+
+            warrior.Rest();
+
+            warrior.GetStatus();
+
+            warrior.Health -= 200;
+
+            warrior.GetStatus();
+        }
+    }
+}
