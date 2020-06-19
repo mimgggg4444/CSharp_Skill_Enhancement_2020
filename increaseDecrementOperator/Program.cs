e70e41a00929c4198fa1824dee4784bc4121c39d
a <a@DESKTOP-231MPSD>
2020-06-20 06:45:44 +0900

    up

---
+++ increaseDecrementOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace increaseDecrementOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            char ch = 'b';//0x62
+
+            ++ch;
+            Console.WriteLine(ch);
+
+            --ch;
+            --ch;
+            --ch;
+
+            Console.WriteLine(ch);
+
+
+        }
+    }
+}
