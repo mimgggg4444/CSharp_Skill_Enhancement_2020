df24bf6590b639be6f6e365395b96226febc8a4d
a <a@DESKTOP-231MPSD>
2020-06-19 12:30:53 +0900

    up

---
+++ LINQupup/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LINQupup
+{
+    public class OrderItem
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public uint Quantity { get; set; }
+    }
+}
