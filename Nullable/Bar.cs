cced5912a15e02cfdacc8739e0b89dbb2f154748
a <a@DESKTOP-231MPSD>
2020-06-19 17:32:32 +0900

    업

---
+++ Nullable/Bar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nullable
+{
+    public struct Bar
+    {
+        public Bar(int number)
+        {
+            Number = number;
+        }
+        public int Number { get; private set; }
+    }
+}
---
+++ Nullable/FOO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nullable
+{
+    public class Foo
+    {
+        public Foo(int number)
+        {
+            Number = number;
+        }
+
+        public int Number { get; private set; }
+
+        public void Increment()
+        {
+            Number++;
+        }
+    }
+}
