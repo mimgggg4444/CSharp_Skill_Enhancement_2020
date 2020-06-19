8ce61b98202408ec4b13aceed1a2fd44979ec8cd
a <a@DESKTOP-231MPSD>
2020-06-20 06:15:42 +0900

    업

---
+++ Add two variables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Add_two_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            // Console.Write(x + "+" + y + "=");
+            // Console.Write(x + y);
+
+            float num1;
+            num1 = 128.512f;
+
+            float num2 = 20.8f;
+            const float PI = 3.14f;
+
+            Console.Write(num1 + "+" + num2 + "=");
+            Console.WriteLine(num1 + num2);
+
+            Console.WriteLine();
+
+            Console.Write(num1 + "-" + num2 + "=");
+            Console.WriteLine(num1 - num2);
+
+            Console.WriteLine();
+
+            Console.Write(num1 + "*" + num2 + "=");
+            Console.WriteLine(num1 * num2);
+
+            Console.WriteLine();
+
+            Console.Write(num1 + "/" + num2 + "=");
+            Console.WriteLine(num1 / num2);
+
+        }
+    }
+}
---
+++ tipsForNamingVariables,constKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tipsForNamingVariables_constKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 28;
+            int b = 20;
+            Console.WriteLine(a + b);
+
+            int appleCount = 17;
+            int orangeCount = 20;
+            */
+
+            const int APPLE_COUNT = 17;
+            const int ORANGE_COUNT = 20;
+
+            Console.WriteLine(APPLE_COUNT + ORANGE_COUNT);
+
+
+        }
+    }
+}
