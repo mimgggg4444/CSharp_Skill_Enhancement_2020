c3ea68cfa223f8a300b234685616815447544426
a <a@DESKTOP-231MPSD>
2020-06-19 11:22:00 +0900

    업

--- Linq1/Program.cs
+++ Linq1/Program.cs
@@ -26,5 +26,6 @@ namespace Linq1
 
 
 
+
     }
 }
---
+++ 함수 오버로딩/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace 함수_오버로딩
+{
+    class Program
+    {
+        
+
+
+
+        static void Print(int score)
+        {
+            Console.WriteLine($"Your score is {score}.");
+        }
+        static void Print(string name)
+        {
+            Console.WriteLine($"Your name is {name}.");
+        }
+        static void Print(float gpa, string name)
+        {
+            Console.WriteLine($"Hello, {name}!\nYour GPA is {gpa}.");
+        }
+
+        static void Main(string[]args)
+        {
+            Print(10);
+            Print("Lulu");
+            Print(89.9f, "Teemo");
+        }
+
+    }
+}
