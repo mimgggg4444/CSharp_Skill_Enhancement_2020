ed699eed450d0d93bfba6ca3730f40de28a96c2e
a <a@DESKTOP-231MPSD>
2020-06-21 08:21:58 +0900

    up

---
+++ incrementAndDecrementOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace incrementAndDecrementOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 10;
+
+            int result1 = num1++ + num2;
+            Console.WriteLine($"{num1} + {num2} = {result1}");
+
+            num1 = 0;
+            num2 = 10;
+
+            int result2 = ++num1 + num2++;
+            Console.WriteLine($"{num1} + {num2} = {result2}");
+
+            num1 = 0;
+            num2 = 10;
+
+            int result3 = num1-- + num1-- - --num2;
+            Console.WriteLine($"{num1} + {num2} = {result3}");
+
+            num1 = 0;
+            num2 = 10;
+
+            int result4 = num2++ * num1++ - --num2 + num1;
+            Console.WriteLine($"{num1} + {num2} = {result4}");
+
+
+        }
+    }
+}
