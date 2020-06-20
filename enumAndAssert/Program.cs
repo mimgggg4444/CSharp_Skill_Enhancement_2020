c55f2761f567f09ed074b28f7a959e6a98a784dd
a <a@DESKTOP-231MPSD>
2020-06-20 15:13:11 +0900

    need Revise

---
+++ enumAndAssert/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace enumAndAssert
+{
+    class Program
+    {
+            enum EDirection
+        {
+            North = 'a',
+            South,
+            East,
+            West
+        };
+        static void Main(string[] args)
+        {
+            EDirection direction;
+            int x = 0;
+            int y = 0;
+
+            EDirection direction2 = EDirection.East;
+            Console.WriteLine(direction2);
+
+            while(true)
+            {
+                Console.WriteLine($"Your location: ({x}, {y})");
+                Console.WriteLine("Where do you want go?(n - 0, s - 1, n - 2, w -3)");
+                direction = (EDirection)int.Parse(Console.ReadLine());
+
+                switch(direction)
+                {
+                    case EDirection.North:
+                        y += 1;
+                        break;
+
+                    case EDirection.South:
+                        y -= 1;
+                        break;
+
+                    case EDirection.East:
+                        x += 1;
+                        break;
+
+                    case EDirection.West:
+                        x -= 1;
+                        break;
+                    default:
+                        Console.WriteLine($"You enter wrong direction!: {direction}");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ enumAndAssertFunction/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace enumAndAssertFunction
+{
+    class Program
+    {
+        
+        static void Move(int direction, ref int x, ref int y)
+        {
+        const int NORTH = 1;
+        const int SOUTH = 2;
+        const int EAST = 3;
+        const int WEST = 4;
+
+            switch (direction)
+            {
+                case NORTH:
+                    y += 1;
+                    break;
+                case SOUTH:
+                    y -= 1;
+                    break;
+                case EAST:
+                    x += 1;
+                    break;
+                case WEST:
+                    x -= 1;
+                    break;
+                default:
+                    Console.WriteLine($"Your enter wrong direction!: {direction}");
+                    break;
+            }
+
+            static void Main(string[] args)
+            {
+                int direction;
+                int x = 0;
+                int y = 0;
+
+                while (true)
+                {
+                    Console.WriteLine($"Your location: ({x}, {y})");
+                    Console.WriteLine("Where do you want go?");
+
+                    direction = int.Parse(Console.ReadLine());
+                    Move(direction, ref x, ref y);
+                }
+            }
+        }
+    }
+}
