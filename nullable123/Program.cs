00d5e423e00e4779279db3f5f9c3b6d59d8cf6b7
a <a@DESKTOP-231MPSD>
2020-06-21 20:10:32 +0900

    up

---
+++ nullable123/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace nullable123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age;
+            age = 17;
+
+            float? height = 180.0f;
+            height = null;
+
+            if(age != null)
+            {
+                Console.WriteLine(age.Value);
+            }
+
+            // int? age = null;
+            if(age == 3)
+            {
+            }
+
+            int? grade = 3;
+            if(grade == 3)
+            {
+            }
+        }
+    }
+}
