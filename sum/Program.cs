d406cce688ea699a741b7b0933502b8bf75defad
a <a@DESKTOP-231MPSD>
2020-06-21 09:12:00 +0900

    up

---
+++ sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX = 100;
+            Console.WriteLine($"sum of 1 - {MAX}");
+
+            int sum = 0;
+            for(int i = 0; i < MAX; i++)
+            {
+                sum += (i + 1);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
