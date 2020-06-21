99bbca52b288119c8be3270d7b20f26b0bb201b3
a <a@DESKTOP-231MPSD>
2020-06-21 10:29:05 +0900

    up

---
+++ Recursivefactiorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Recursivefactiorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const ulong FACTORIAL = 10;
+
+            Console.WriteLine(NonRecursiverFactorial(FACTORIAL));
+            Console.WriteLine();
+            Console.WriteLine(RecursiverFactorial(FACTORIAL));
+
+
+        }
+
+        static ulong NonRecursiverFactorial(ulong n)
+        {
+            if(n <= 1)
+            {
+                return 1;
+            }
+
+            uint factorial = 1;
+            for(uint i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        static ulong RecursiverFactorial(ulong n)
+        {
+            if(n == 0)
+            {
+                return 1;
+            }
+
+            return RecursiverFactorial(n - 1) * n;
+        }
+    }
+}
