67c19d4c741e276513ddf70e9e5377474d1f51e3
a <a@DESKTOP-231MPSD>
2020-06-27 18:10:04 +0900

    up

---
+++ bitFlag1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace bitFlag1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {x
+            const byte CC_ASLEEP = 0;
+            const byte CC_AIRBORNE = 1;
+            const byte CC_BLIND = 2;
+            const byte CC_STUN = 3;
+            // const byte CC_NONE = 0;
+
+
+            byte myChampStatus = CC_ASLEEP;
+
+            byte m1 = (1 << CC_BLIND);
+            myChampStatus |= m1;
+
+            byte m2 = (1 << CC_STUN) | (1 << CC_AIRBORNE);
+            myChampStatus |= m2;
+
+            byte m3 = (1 << CC_STUN);
+            myChampStatus &= (byte)~m3;
+
+            Console.WriteLine(Convert.ToString(myChampStatus, 2).PadLeft(8, '0'));
+
+        }
+    }
+}
