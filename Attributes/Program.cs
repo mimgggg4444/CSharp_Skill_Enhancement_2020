a3ce8b46d2216f18dc7c22f31dcbf143724a614b
a <a@DESKTOP-231MPSD>
2020-07-04 12:47:34 +0900

    up

---
+++ Attributes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Attributes
+{
+
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            MyUIClass
+        }
+    }
+    public class MyUIClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler propertychanged;
+
+        public void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            propertychanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(value != _name)
+                {
+                    _name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+    
+        
+    }
+    public class MyClass : Attribute
+    {
+    }
+
+    public class SomeOtherClass
+    {
+    }
+
+    public class AttributeFile
+    {
+    }
+}
---
+++ CopyDirectory01/CopyDirectory01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectory01
+{
+    public class Program
+    {
+        public static readonly string INPUT_DIRECTORY = Path.Combine(Directory.GetCurrentDirectory(), "input");
+        public static readonly string OUTPUT_DIRECTORY = Path.Combine(Directory.GetCurrentDirectory(), "output");
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        private static void copyDirectory(string sourcePath, string destPath)
+        {
+            var files = Directory.GetFiles(sourcePath);
+
+            foreach(var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                File.Copy(file, Path.Combine(destPath, fileName));
+            }
+
+            var directories = Directory.GetDirectories(sourcePath);
+
+            foreach(var directory in directories)
+            {
+                var dest = directory.Replace(sourcePath, destPath);
+                Directory.CreateDirectory(dest);
+                copyDirectory(directory, dest);
+            }
+        }
+
+        private static void cleanDirectory(string path)
+        {
+            if(Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+
+                foreach(string filepath in files)
+                {
+                    File.Delete(filepath);
+                }
+
+                string[] directories = Directory.GetDirectories(path);
+
+                foreach(string directoryPath in directories)
+                {
+                    cleanDirectory(directoryPath);
+                }
+
+                Directory.Delete(path);
+            }
+        }
+    }
+}
