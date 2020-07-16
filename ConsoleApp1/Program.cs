2dc8193feaeaedad4b19a3c1c1ecbb65e7e20832
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-16 19:20:05 +0900

    Add files via upload
    up

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User count: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] ages = new int[length];
+            float[] heights = new float[length];
+
+            int totalAges = 0;
+            float totalHeights = 0.0f;
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write($"Enter user {i}'s age: ");
+                ages[i] = int.Parse(Console.ReadLine());
+                totalAges += ages[i];
+
+                Console.Write($"Enter user {i}'s height(cm): ");
+                heights[i] = float.Parse(Console.ReadLine());
+                totalHeights += heights[i];
+            }
+
+            Console.WriteLine("Print result ----- ");
+            for (int i = 0; i < length; ++i)
+            {
+                Console.WriteLine($"User{i}: Age({ages[i]}), Height({heights[i]}cm)");
+            }
+
+            Console.WriteLine($"Total ages: {totalAges}");
+            Console.WriteLine($"Total heights: {totalHeights}");
+        }
+    }
+}
