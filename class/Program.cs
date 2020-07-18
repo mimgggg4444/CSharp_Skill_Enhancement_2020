799471a1e403f9497ab8966bcc486dd569fcd84f
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-18 18:39:51 +0900

    Add files via upload
    up

--- class/Program.cs
+++ class/Program.cs
@@ -1,31 +1,29 @@
-﻿using class1;
-using System;
-using System.Net.Http.Headers;
-using System.Xml.Serialization;
+﻿using System;
 
-namespace classPractice
+namespace classtest
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+            Warrior warrior = new Warrior("kim!");
 
+            warrior.Introduce();
+            warrior.GetStatus();
 
+            warrior.SwordStrike();
 
-            Car mycar = new Car(Car.EGasType.Diesel, 1000000);
-            int newPrice;
-            Console.WriteLine("Hello?");
-            int.TryParse(Console.ReadLine(), out newPrice);
-            mycar.SetPrice(newPrice);
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
 
-            float remainingFulel = mycar.GetGas();
-            Console.WriteLine($"Remaining fuel: {remainingFulel}L");
+            warrior.Rest();
 
+            warrior.GetStatus();
 
+            warrior.Health -= 200;
 
-
-            // float remaingFuel = mycar.mGas; Due to the level of protection, the fuel level is unknown.
-
-        }
+            warrior.GetStatus();
     }
+}
 }
---
+++ class/Warrior.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace classtest
+{
+    class Warrior
+    {
+        private const int MAX_HEALTH = 500;
+        private const int MAX_MANA = 50;
+        private const int MAX_STAMINA = 200;
+
+        private int mHEALTH = MAX_HEALTH;
+        private int mMANA = MAX_MANA;
+        private int mSTAMINA = MAX_STAMINA;
+
+        public Warrior(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public int Health
+        {
+            get
+            {
+                return mHEALTH;
+            }
+            set
+            {
+                mHEALTH = value;
+                
+                if(mHEALTH < 0)
+                {
+                    mHEALTH = 0;
+                }
+
+                if(mHEALTH > MAX_HEALTH)
+                {
+                    mHEALTH = MAX_HEALTH;
+                }
+            }
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return mMANA;
+            }
+            set
+            {
+                mMANA = value;
+
+                if(mMANA < 0)
+                {
+                    mMANA = 0;
+                }
+                if(mMANA > MAX_MANA)
+                {
+                    mMANA = MAX_MANA;
+                }
+            }
+        }
+
+        public int Stamina
+        {
+            get
+            {
+                return mSTAMINA;
+            }
+            set
+            {
+                mSTAMINA = value;
+
+                if(mSTAMINA < 0)
+                {
+                    mSTAMINA = 0;
+                }
+                if(mSTAMINA > MAX_STAMINA)
+                {
+                    mSTAMINA = MAX_STAMINA;
+                }
+            }
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"I'm {Name}! Honour and glory!! THOS OS SPARTA!!");
+        }
+
+        public void UseWhirlwind()
+        {
+            if(Mana < 5 || Stamina < 70)
+            {
+                Console.WriteLine($"{Name} con't use Whirlwind!");
+                return;
+            }
+
+            Mana -= 5;
+            Stamina -= 70;
+
+            Console.WriteLine($"{Name} used Whilrwind!");
+        }
+
+        public void SwordStrike()
+        {
+            if(Stamina < 20)
+            {
+                Console.WriteLine($"{Name} con't use SwordStrike! ");
+                return;
+            }
+
+            Stamina -= 20;
+            Console.WriteLine($"{Name} used SwordStrike! ");
+        }
+
+        public void Rest()
+        {
+            Mana += 2;
+            Stamina += 5;
+            Health += 5;
+
+            Console.WriteLine($"{Name} is resting");
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine($"{Name} - {Health} - {Mana} - {Stamina}");
+        }
+    }
+
+
+}
