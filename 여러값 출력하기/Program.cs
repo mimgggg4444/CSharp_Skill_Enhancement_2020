8f551d68f3bbc50ab4e21c942835771e523fb958
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-25 11:17:39 +0900

    Add files via upload
    up

---
+++ 여러값 출력하기/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 여러값_출력하기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(30);
+            Console.WriteLine(30.1);
+            Console.WriteLine(30.2f);
+
+            Console.Write(true);
+            Console.Write('p');
+
+            //
+
+            /*
+             */
+        }
+    }
+}
